eb7fa37487a58817349007428c7e29fa8ea644bc
nhathuyenqt <xuhelios@gmail.com>
2021-11-07 13:32:17 +0100

    people

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import People from '../views/People.vue'
+import Contact from '../views/Contact.vue'
 import Vue from "vue";
 import Router from "vue-router";
 
@@ -23,7 +24,8 @@ const routes = [
     },
     {
         path: '/contact',
-        name: 'Contact'
+        name: 'Contact',
+        component: Contact
     }
 ]
 const router = new VueRouter({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,13 @@
+.card{
+  background-color: #bada55;
+  flex-grow:1;
+  margin: 10px;
+  height: 50px;
+}
+
 <template>
     <div>
-        <v-card class="mx-auto">
+        <v-card class="mx-auto" margin="10px" >
             <v-card-text>
                 <div>Word of the Day</div>
                 
@@ -39,7 +46,7 @@
                 small
                 >
                 <v-row class="pt-1">
-                    <v-col cols="3">
+                    <v-col cols="2">
                     <strong>May 2021</strong>
                     </v-col>
                     <v-col>
@@ -56,7 +63,7 @@
                 small
                 >
                 <v-row class="pt-1">
-                    <v-col cols="3">
+                    <v-col cols="2">
                     <strong>April 2021</strong>
                     </v-col>
                     <v-col>
@@ -71,7 +78,7 @@
                     color="pink" small
                 >
                 <v-row class="pt-1">
-                    <v-col cols="3">
+                    <v-col cols="2">
                         <strong>Feb. 2021</strong>
                     </v-col>
                     <v-col>
@@ -87,7 +94,7 @@
                     small
                 >
                 <v-row class="pt-1">
-                    <v-col cols="3">
+                    <v-col cols="2">
                     <strong>Feb. 2021</strong>
                     </v-col>
                     <v-col>
@@ -101,8 +108,98 @@
             </v-timeline>
             </v-card-text>
         </v-card>
+        
+        <v-card class="mx-auto" margin="10px" >
+            <v-card-title >
+            
+                <h3 class="text-h6 font-weight-light text-left grow">
+                    Recent publications
+                </h3>
+               
+            </v-card-title>
+            <v-list three-line>
+            <template v-for="(item, index) in items">
+                <v-subheader
+                v-if="item.header"
+                :key="item.header"
+                v-text="item.header"
+                ></v-subheader>
+
+                <v-divider
+                v-else-if="item.divider"
+                :key="index"
+                :inset="item.inset"
+                ></v-divider>
+
+                <v-list-item
+                v-else
+                :key="item.title"
+                >   
+                    <v-col cols="2">
+                        <v-img :src="item.avatar"></v-img>
+                    </v-col>
+                    <v-col>
+                        <v-list-item-content>
+                            <v-list-item-title v-html="item.authors"></v-list-item-title>
+                            <v-list-item-title v-html="item.title" class="font-weight-medium"></v-list-item-title>
+                            <v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
+                        </v-list-item-content>
+                    </v-col>
+                
+                </v-list-item>
+            </template>
+            </v-list>
+        </v-card>
     </div>
 </template>
+
+
+
+
+
+<script>
+  export default {
+    data: () => ({
+      items: [
+        // { header: 'Recent publications' },
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+          authors: 'B. Liang, G. Banegas, A. Mitrokotsa',
+          title: 'Statically Aggregate Verifiable Random Functions and Application to E-Lottery.',
+          subtitle: 'Cryptograhy (MDPI), 4(4), 37, doi:10.3390/cryptography4040037, 2020.',
+        },
+        { divider: true, inset: true },
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
+          authors: 'G. Tsaloli, G. Banegas, A. Mitrokotsa',
+          title: 'Practical and Provably Secure Distributed Aggregation: Verifiable Additive Homomorphic Secret Sharing.',
+          subtitle:'Cryptograhy (MDPI), 4(3), 25, doi:10.3390/cryptography4030025, 2020.',
+        },
+        { divider: true, inset: true },
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
+          authors: 'D. Fiore, A. Mitrokotsa, L. Nizzardo, E. Pagnin.',
+          title: 'Multi-key homomorphic authenticators.',
+          subtitle: 'IET Information Security, doi:10.1049/iet-ifs.2018.5341, 2019. ðŸ† [ IET Premium Award ] (Best paper published within the last two years in the IET Information Security Journal)',
+        },
+        { divider: true, inset: true },
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
+          authors: 'B. Liang, A. Mitrokotsa.',
+          title: 'Robust Distributed Pseudorandom Functions for mNP Access Structures.',
+          subtitle:'In Proceedings of International Conference on Information Security (ISC 2019), pp 107-126, LNCS, New York, US, Sept. 2019. ðŸ† Best Paper Award!',
+        },
+        { divider: true, inset: true },
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
+          authors: 'C. Brunetta, B. Liang, A. Mitrokotsa.',
+          title: 'Code-Based Zero Knowledge PRF Arguments.',
+          subtitle: 'In Proceedings of International Conference on Information Security (ISC 2019), pp 171-189, LNCS, New York, US, Sept. 2019.',
+        },
+      ],
+    }),
+  }
+</script>
 
 
 
