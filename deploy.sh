06ae7887066586f0099405427e30bf942de6c9c3
nhathuyenqt <xuhelios@gmail.com>
2021-12-07 14:20:42 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f https://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@github.com:nhathuyenqt/k-web.git master:gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
