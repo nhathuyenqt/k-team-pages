675f4afd615040cee6a779494ac85df559ef1007
Nhat Xu <nhat_huyen@macpro.local>
2022-03-23 18:22:22 +0700

    remove meta

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -116,7 +116,7 @@
                 <template v-for="(item) in recentPub">
                 <v-list-item :key="item.id">
                     <v-col cols="2" class="pl-10">
-                            <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+                
                             <iframe height="120px" width="90px" :src="item.paperLink" allow="autoplay"></iframe>
                     </v-col>
                     <v-col>
@@ -153,7 +153,7 @@ import axios from 'axios';
           axios.get('https://cyber-api.hellven.io/publications').then(response => {
             this.journals = response.data;
             this.recentPub = response.data.slice(response.data.length-10,(response.data.length))
-            console.log(this.recentPub)
+           // console.log(this.recentPub)
           }); 
         }
     }
